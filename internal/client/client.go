1581e74be99dae02757074136463a7cff6ff7fc2
0ya-sh0 <ymthatte.gmail.com>
2026-02-01 10:02:08 +0530

    UI improvements 1. chat screen scroll 2. active/inactive markers 3. unread counts

--- internal/client/client.go
+++ internal/client/client.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/0ya-sh0/GoChatTUI/internal/server"
 	"github.com/gorilla/websocket"
+	"golang.org/x/term"
 )
 
 func connect(userName string, url url.URL) (*websocket.Conn, error) {
@@ -158,17 +159,43 @@ func printHeader(userName string) {
 	fmt.Print(Reset)
 }
 
-func printUserName(userName string) {
+func printUserName(userName string, activeUsers map[string]bool) {
 	fmt.Printf(CursorPos, 4, 1)
-	fmt.Print("(<- CTRL-C)   ", userName)
+	fmt.Print("(<- CTRL-C)   ")
+	if _, ok := activeUsers[userName]; ok {
+		fmt.Print(FgGreen, " ● ")
+	} else {
+		fmt.Print(FgGreen, " ○ ")
+	}
+	fmt.Print(userName)
 	fmt.Printf(CursorPos, 5, 1)
 	fmt.Print("----------")
 }
 
-func printCurrentText(currentText string) {
-	fmt.Printf(CursorPos, 20, 1)
+func listenResizeEvents(events chan int) {
+	_, height, err := term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		close(events)
+		return
+	}
+	for {
+		time.Sleep(time.Second)
+		_, newHeight, err := term.GetSize(int(os.Stdin.Fd()))
+		if err != nil {
+			close(events)
+			return
+		}
+		if newHeight != height {
+			height = newHeight
+			events <- height
+		}
+	}
+}
+
+func printCurrentText(currentText string, height int) {
+	fmt.Printf(CursorPos, height-1, 1)
 	fmt.Print(Reset, "----------")
-	fmt.Printf(CursorPos, 21, 1)
+	fmt.Printf(CursorPos, height, 1)
 	if currentText == "" {
 		fmt.Print(BgBlack, FgMagenta, "Enter text... (Enter) to send")
 	} else {
@@ -177,32 +204,36 @@ func printCurrentText(currentText string) {
 	fmt.Print(Reset)
 }
 
-func printUsers(users []string, userPos int) {
+func printUsers(users []string, userPos int, messages map[string]ChatData) {
 	i := 6
 	for pos, v := range users {
 		fmt.Print(Reset)
 		if pos == userPos {
 			fmt.Printf(CursorPos, i, 1)
-			fmt.Print(Bold, BgBlack, FgRed, "> ")
+			fmt.Print(Bold, BgBlack, FgRed, "▶ ")
 		} else {
 			fmt.Printf(CursorPos, i, 3)
 		}
 		i++
-		fmt.Print(v)
+		fmt.Print(FgRed, v, " (", messages[v].unread, ")")
 	}
 	fmt.Print(Reset)
 }
 
-func printMessages(userName string, data ChatData) {
-	i := 6
-	for _, v := range data.messages {
+func printMessages(userName string, data ChatData, height, messageScroll int) {
+	line := 6
+	start := messageScroll
+	end := min(len(data.messages), messageScroll+(height-7))
+	for curr := start; curr < end; curr++ {
+		v := data.messages[curr]
 		fmt.Print(Reset)
-		fmt.Printf(CursorPos, i, 1)
-		i++
+		fmt.Printf(CursorPos, line, 1)
+		line++
+		v.Timestamp.Format(time.RFC822)
 		if v.FromUserName == userName {
-			fmt.Print(FgGreen, "(You): ", Reset, v.Content)
+			fmt.Print(FgGreen, v.Timestamp.Format(time.DateOnly+" "+time.TimeOnly), " ", "(You): ", Reset, v.Content)
 		} else {
-			fmt.Print(FgRed, v.FromUserName, ": ", Reset, v.Content)
+			fmt.Print(FgRed, v.Timestamp.Format(time.DateOnly+" "+time.TimeOnly), " ", v.FromUserName, ": ", Reset, v.Content)
 		}
 	}
 }
@@ -228,30 +259,32 @@ func Start(userName string, url url.URL) error {
 
 	keyEvents := make(chan EventKeyPress)
 	wsEvents := make(chan server.MessageToClient)
-
+	resizeEvents := make(chan int)
+	messageScroll := 0
 	messages := make(map[string]ChatData)
 	users := []string{}
+	activeUsers := make(map[string]bool)
 	userPos := 0
 	isMainScreen := true
 	chosenUser := ""
 	currentText := ""
+	currentChatData := ChatData{}
+	_, height, err := term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		return err
+	}
 	go listenKeyEvents(keyEvents)
 	go listenWSEvents(wsEvents, conn)
+	go listenResizeEvents(resizeEvents)
 	for {
 		fmt.Print(ClearScreen, CursorHome, CursorHide)
 		printHeader(userName)
 		if isMainScreen {
-			printUsers(users, userPos)
+			printUsers(users, userPos, messages)
 		} else {
-			printUserName(chosenUser)
-			data, ok := messages[chosenUser]
-			if !ok {
-				data = ChatData{}
-			}
-			data.unread = 0
-			messages[chosenUser] = data
-			printMessages(userName, data)
-			printCurrentText(currentText)
+			printUserName(chosenUser, activeUsers)
+			printMessages(userName, currentChatData, height, messageScroll)
+			printCurrentText(currentText, height)
 		}
 		select {
 		case event, ok := <-keyEvents:
@@ -266,20 +299,58 @@ func Start(userName string, url url.URL) error {
 				userPos = 0
 				currentText = ""
 			}
+			/*
+
+				available height for messages = height - 7
+				number of messages = len(messages)
+				scroll pos determines what first message is seen
+				i.e if we want to show message i to i + (height-7)
+				scroll is i
+				lowerbound i can be 0 (show first message)
+				upperbound
+					say we have height 20 and 5 messages, we cant scroll, i remains 0
+
+					if we h = 20, m = 20, i remains 0
+					h = 20, m = 21, i 0 or 1
+					h = 20, m = 22, i 0, 1, 2
+
+					h = 20, m = 100, i 0 to 80
+
+					if h >= m, upperbound = 0
+					else upperbound = m - (h - 7)
+
+				or i can be min(len(messages), )
+
+
+			*/
 			if event.KeyType == KEY_TYPE_UP_ARROW {
-				if len(users) > 0 {
+				if isMainScreen && len(users) > 0 {
 					userPos = (len(users) + userPos - 1) % len(users)
+				}
+				if !isMainScreen && len(currentChatData.messages) > (height-7) {
+					if messageScroll > 0 {
+						messageScroll--
+					}
 				}
 			}
 			if event.KeyType == KEY_TYPE_DOWN_ARROW {
-				if len(users) > 0 {
+				if isMainScreen && len(users) > 0 {
 					userPos = (len(users) + userPos + 1) % len(users)
+				}
+				if !isMainScreen && len(currentChatData.messages) > (height-7) {
+					if messageScroll < len(currentChatData.messages)-(height-7) {
+						messageScroll++
+					}
 				}
 			}
 			if event.KeyType == KEY_TYPE_ENTER {
 				if isMainScreen && len(users) > 0 {
 					chosenUser = users[userPos]
 					isMainScreen = false
+					data := messages[chosenUser]
+					data.unread = 0
+					messages[chosenUser] = data
+					currentChatData = data
 				}
 				if !isMainScreen && len(currentText) > 0 {
 					data := messages[chosenUser]
@@ -293,6 +364,10 @@ func Start(userName string, url url.URL) error {
 					messages[chosenUser] = data
 					currentText = ""
 					conn.WriteJSON(localMessage)
+					currentChatData = data
+					if len(currentChatData.messages) > (height - 7) {
+						messageScroll = len(currentChatData.messages) - (height - 7)
+					}
 				}
 
 			}
@@ -313,6 +388,7 @@ func Start(userName string, url url.URL) error {
 				return nil
 			}
 			if event.Type == server.BROADCAST_TYPE {
+				activeUsers = make(map[string]bool)
 				users = event.Users
 				if len(users) <= 1 {
 					users = []string{}
@@ -322,17 +398,18 @@ func Start(userName string, url url.URL) error {
 						if v == userName {
 							continue
 						}
+						if _, ok := messages[v]; !ok {
+							messages[v] = ChatData{}
+						}
 						filtered = append(filtered, v)
+						activeUsers[v] = true
 					}
 					users = filtered
 					sort.Strings(users)
 				}
 			}
 			if event.Type == server.CHAT_TYPE {
-				data, ok := messages[event.FromUserName]
-				if !ok {
-					data = ChatData{}
-				}
+				data := messages[event.FromUserName]
 				data.messages = append(data.messages, Message{
 					FromUserName: event.FromUserName,
 					ToUserName:   userName,
@@ -341,9 +418,20 @@ func Start(userName string, url url.URL) error {
 				})
 				if isMainScreen || chosenUser != event.FromUserName {
 					data.unread++
+					messages[event.FromUserName] = data
+				} else {
+					messages[event.FromUserName] = data
+					currentChatData = data
+					if len(currentChatData.messages) > (height - 7) {
+						messageScroll = len(currentChatData.messages) - (height - 7)
+					}
 				}
-				messages[event.FromUserName] = data
 			}
+		case event, ok := <-resizeEvents:
+			if !ok {
+				return nil
+			}
+			height = event
 		}
 	}
 }
